a13b2c02b496edb30fb418464dd11c1e2b0e111c
soumyabhoj <soumya.daya@gmail.com>
2024-02-16 06:45:07 +0100

    first commit

---
+++ src/test/java/com/example/demoactions/DemoActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
